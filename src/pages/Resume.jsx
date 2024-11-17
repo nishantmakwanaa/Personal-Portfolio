e2bc4635ebedeb17a627f7e78c19c275440b1043
Nishant Makwana <144738854+nishantmakwanaa@users.noreply.github.com>
2024-11-17 14:15:12 +0530

    Update Resume.JSX

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -7,7 +7,7 @@ const Resume = () => {
       <div className="skills__container bd-grid home-position">
         <div className="work__img">
           <a
-            href="https://drive.google.com/file/d/1cyrMa5We4LJWO1Z4RFo8TpxXPi-T0sO0/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1l5bdBPI6YDx1kQBuY0VDV_NYYMb-IIAR/view?usp=sharing"
             target="_blank"
           >
             <img src={resume} alt="" className="skills__img" />
